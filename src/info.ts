7b1627667786c4730a1136e469235163d1a93493
Nayla <naylahanegan@gmail.com>
2023-05-20 13:14:05 -0400

    Update info.ts

--- src/info.ts
+++ src/info.ts
@@ -1,22 +1,26 @@
 export const information = {
     // What's your GitHub username?
-    username: "cnrad",
+    username: "EndangeredNayla",
 
     // Highlight some of your best works by providing your repository NAMES.
-    featuredRepos: ["lanyard-profile-readme", "d", "iridi", "h", "sponsor", "hbd-today"],
+    featuredRepos: []
 
     // Describe yourself, and why you should be sponsored.
     description: `
-👋 Hey there, I'm Conrad. I'm a high-school student currently pursuing full-stack web development in my free time.
+👋 Hey there, I'm Nayla.
+
+My username on most of the websites I use is EndangeredNayla.
+
+I am mostly intested in Block-Game modding, Nintendo, Windows Batch files, and Toontown. I do have a lot of other interests though!
+
+
     
-I enjoy leveraging technologies such as TypeScript, Next.js, Postgres, Prisma, and many others to create scalable and performant web applications, and I'm constantly looking for projects to develop and things to learn in order to expand my knowledge and skillset further.
-    
-If you choose to donate (any amount is appreciated), the funds will help cover expenses such as domain names, server hosting, and equipment to maximize my coding workflow. Feel free to hit me up and let me know you've donated as well, I'd be more than happy to feature you on my GitHub profile or projects as a little "thank you" 👍
+If you choose to donate (any amount is appreciated), the funds will help cover expenses such as domain names, server hosting, and equipment to maximize my development workflow. Feel free to hit me up and let me know you've donated as well 👍
     `,
 
     // Where should they be directed to pay you?
-    payment_url: "https://pay.cnrad.dev",
+    payment_url: "https://gl.me/u/LwlcBjNlRHTW",
 
     // Only set to true if your URL supports an amount at the end, e.g. https://paypal.com/paypalme/username/20, otherwise set it to false.
-    amount_slug: true,
+    amount_slug: false,
 };
